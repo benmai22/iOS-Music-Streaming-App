d92b1db6267966f0fb82f327cdbc78641d2d7964
benmai22 <bcmai@cpp.edu>
2021-04-02 12:22:00 -0700

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by benjamin mai on 4/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
